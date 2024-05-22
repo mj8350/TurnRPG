27d983ad3746c09475e0ab61afb633d62d5584f9
dbstjr5867 <157046595+dbstjr5867@users.noreply.github.com>
2024-05-22 16:55:26 +0900

    스크립터블 테스트중

---
+++ Assets/YS/YSCharData.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "YSChar Data", menuName = "Scriptable Object/YSChar Data", order = int.MaxValue)]
+public class YSCharData : ScriptableObject
+{
+    [SerializeField]
+    private string CharName;
+    public string ZombieName { get { return CharName; } }
+    [SerializeField]
+    private int hp;
+    public int Hp { get { return hp; } }
+    [SerializeField]
+    private int damage;
+    public int Damage { get { return damage; } }
+    [SerializeField]
+    private float sightRange;
+    public float SightRange { get { return sightRange; } }
+    [SerializeField]
+    private float moveSpeed;
+    public float MoveSpeed { get { return moveSpeed; } }
+}
