8dadbf2e8eb808c96bd8dda1aacffaacd7811fc1
dbstjr5867 <157046595+dbstjr5867@users.noreply.github.com>
2024-05-23 15:50:00 +0900

    캐릭터 정보창UI만듬

--- Assets/YS/YSChar.cs
+++ Assets/YS/YSChar.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "YSChar Data", menuName = "Scriptable Object/YSChar Data", order = int.MaxValue)]
+
 public class YSChar : MonoBehaviour
 {
     [SerializeField]
@@ -11,7 +11,7 @@ public class YSChar : MonoBehaviour
 
     public void WatchCharInfo()
     {
-        Debug.Log("캐릭터 이름 :: " + yscharData.ZombieName);
+        Debug.Log("캐릭터 이름 :: " + yscharData.CharName);
         Debug.Log("캐릭터 체력 :: " + yscharData.Hp);
         Debug.Log("캐릭터 공격력 :: " + yscharData.Damage);
         Debug.Log("캐릭터 시야 :: " + yscharData.SightRange);
