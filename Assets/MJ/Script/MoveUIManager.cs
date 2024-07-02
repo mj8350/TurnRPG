0fb335fa3f44ecf2ce036f41a9f2662599eab326
mj8350 <76643496+mj8350@users.noreply.github.com>
2024-07-02 16:07:02 +0900

    이동씬UI연동

--- Assets/MJ/Script/MoveUIManager.cs
+++ Assets/MJ/Script/MoveUIManager.cs
@@ -9,6 +9,23 @@ public class MoveUIManager : MonoBehaviour
     public TextMeshProUGUI DiceText;
     public TextMeshProUGUI PointText;
 
+    public TextMeshProUGUI[] LevelUI;
+    public TextMeshProUGUI[] PlayerName;
+    public TextMeshProUGUI[] HPText;
+    public TextMeshProUGUI[] EXPText;
+
+    public Image[] Profile;
+    public Slider[] HPSlider;
+    public Slider[] EXPSlider;
+
+    public Sprite[] profileSP;
+
+    private void Awake()
+    {
+        EXPChange();
+        HPChange();
+        ProfileChange();
+    }
 
 
     public void D_TextChange()
@@ -20,4 +37,41 @@ public class MoveUIManager : MonoBehaviour
     {
         PointText.text = GameManager.Instance.movePoint.ToString();
     }
+
+    public void EXPChange()
+    {
+        int MaxEXP, CurEXP;
+        for (int i = 0; i < 3; i++)
+        {
+            if (GameManager.Instance.PlayerLevel < 10)
+            {
+                MaxEXP = GameManager.Instance.MaxEXP[GameManager.Instance.PlayerLevel - 1];
+                CurEXP = GameManager.Instance.PlayerExp;
+                EXPSlider[i].value = ((float)CurEXP / MaxEXP);
+                EXPText[i].text = $"{CurEXP}/{MaxEXP}";
+                LevelUI[i].text = GameManager.Instance.PlayerLevel.ToString();
+            }
+        }
+    }
+    public void HPChange()
+    {
+        int Maxhp, Curhp;
+        for (int i = 0; i < 3; i++)
+        {
+            Maxhp = GameManager.Instance.player[i].MaxHP;
+            Curhp = GameManager.Instance.player[i].CurHP;
+            HPSlider[i].value = ((float)Curhp / Maxhp);
+            HPText[i].text = $"{Curhp}/{Maxhp}";
+        }
+    }
+
+    public void ProfileChange()
+    {
+        for(int i = 0;i < 3; i++)
+        {
+            Profile[i].sprite = profileSP[GameManager.Instance.player[i].id];
+            PlayerName[i].text = GameManager.Instance.player[i].CharName;
+        }
+    }
+
 }
